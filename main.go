6ee7fe29632dee5f44ada7ec66523157d5be2205
xrstf <git@xrstf.de>
2022-03-12 18:20:32 +0100

    fix handling fully specified kind names

--- main.go
+++ main.go
@@ -195,12 +195,19 @@ func watchKubernetes(ctx context.Context, log logrus.FieldLogger, args []string,
 	for _, resourceKind := range resourceKinds {
 		log.Debugf("Resolving %s...", resourceKind)
 
-		gvk, err := mapper.KindFor(schema.GroupVersionResource{Resource: resourceKind})
+		parsed, err := mappingFor(mapper, resourceKind)
 		if err != nil {
 			log.Fatalf("Unknown resource kind %q: %v", resourceKind, err)
 		}
 
+		gvk := parsed.GroupVersionKind
 		kinds[gvk.String()] = gvk
+
+		log.WithFields(logrus.Fields{
+			"group":   gvk.Group,
+			"version": gvk.Version,
+			"kind":    gvk.Kind,
+		}).Debug("Resolved")
 	}
 
 	// setup watches for each kind
@@ -275,4 +282,48 @@ func getDynamicInterface(gvk schema.GroupVersionKind, namespace string, dynamicC
 	}
 
 	return dr, nil
+}
+
+// mappingFor is copied straight from kubectl:
+// https://github.com/kubernetes/kubernetes/blob/0b8d725f5a04178caf09cd802305c4b8370db65e/staging/src/k8s.io/cli-runtime/pkg/resource/builder.go
+func mappingFor(restMapper meta.RESTMapper, resourceOrKindArg string) (*meta.RESTMapping, error) {
+	fullySpecifiedGVR, groupResource := schema.ParseResourceArg(resourceOrKindArg)
+	gvk := schema.GroupVersionKind{}
+
+	if fullySpecifiedGVR != nil {
+		gvk, _ = restMapper.KindFor(*fullySpecifiedGVR)
+	}
+	if gvk.Empty() {
+		gvk, _ = restMapper.KindFor(groupResource.WithVersion(""))
+	}
+	if !gvk.Empty() {
+		return restMapper.RESTMapping(gvk.GroupKind(), gvk.Version)
+	}
+
+	fullySpecifiedGVK, groupKind := schema.ParseKindArg(resourceOrKindArg)
+	if fullySpecifiedGVK == nil {
+		gvk := groupKind.WithVersion("")
+		fullySpecifiedGVK = &gvk
+	}
+
+	if !fullySpecifiedGVK.Empty() {
+		if mapping, err := restMapper.RESTMapping(fullySpecifiedGVK.GroupKind(), fullySpecifiedGVK.Version); err == nil {
+			return mapping, nil
+		}
+	}
+
+	mapping, err := restMapper.RESTMapping(groupKind, gvk.Version)
+	if err != nil {
+		// if we error out here, it is because we could not match a resource or a kind
+		// for the given argument. To maintain consistency with previous behavior,
+		// announce that a resource type could not be found.
+		// if the error is _not_ a *meta.NoKindMatchError, then we had trouble doing discovery,
+		// so we should return the original error since it may help a user diagnose what is actually wrong
+		if meta.IsNoMatchError(err) {
+			return nil, fmt.Errorf("the server doesn't have a resource type %q", groupResource.Resource)
+		}
+		return nil, err
+	}
+
+	return mapping, nil
 }
